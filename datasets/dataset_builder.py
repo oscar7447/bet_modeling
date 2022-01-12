d6a85d56d0c9b03a0019cbc472ae12dc036839d0
oscar7447 <omleon7447@gmail.com>
2022-01-12 10:28:24 -0500

    delete dummy files

--- datasets/dataset_builder.py
+++ datasets/dataset_builder.py
@@ -2,7 +2,7 @@ from features.feature_factory import FeatureFactory
 import pandas as pd
 from datasets.game_dataset import game_dataset_builder
 if __name__=='__main__':
-
+#[TODO] PASS DF FOR EACH GAME WITH THE BASIC INFO: HOME-AWAY TEAM, MATCHWEEK, SEASON, COMPETITION 
 
     matchhistorical = pd.read_pickle('files/match_historical_data_v2.pkl')
     df_games = game_dataset_builder()
